50c6a8ca29c12ea940bfd78d2cfcbe351f532d2c
roset nathan <rsn3112a@ens.fsi>
2022-09-27 19:19:51 +0200

    déclaration de la classe Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,35 @@
 package personnages;
 
 public class Romain {
+	private String nom;
+	private int force;
+	
+	
+	public String getNom() {
+		return nom;
+	}
 
+
+	public Romain(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+	}
+
+	private String prendreParole() {
+		return "Le romain" + nom + ":";
+	}
+	
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	}
 }
