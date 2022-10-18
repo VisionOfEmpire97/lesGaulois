e9ad56ad4295658be045114f1cd7b4d3f8d4d4cb
Nathan <nathroset97@gmail.com>
2022-10-18 14:51:14 +0200

    début TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,10 +1,32 @@
 package personnages;
 
 public class Village {
-
-	public String getNom() {
-		// TODO Auto-generated method stub
-		return null;
+	private String nom;
+	private Chef chef;
+	private String[] villageois;
+	private int nbVillageois = 0;
+	
+	public Village(String nom, int nbVillageoisMaximum) {
+		super();
+		this.nom = nom;
+		this.villageois[nbVillageois] = villageois[nbVillageoisMaximum];
 	}
 
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		Village.villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
+	
+	public void trouverHabitant(int numero) {
+		System.out.println(Village.villageois[numero]);
+	}
 }
