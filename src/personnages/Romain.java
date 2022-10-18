b773ebaa7851bd510f95d892c99daa9b55aaae5d
Nathan <nathroset97@gmail.com>
2022-10-18 16:52:17 +0200

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,23 +4,20 @@ public class Romain {
 	private String nom;
 	private int force;
 	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+		assert force >= 0: "force doit être positive";
+	}
 	
 	public String getNom() {
 		return nom;
 	}
 
-
 	public int getForce() {
 		return force;
 	}
 
-
-	public Romain(String nom, int force) {
-		super();
-		this.nom = nom;
-		this.force = force;
-	}
-	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<< " + texte + " >>");
 	}
@@ -30,13 +27,23 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert force >= 0: "force doit être positive";
+		int forceBuff = force; 
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert forceBuff > force: "force doit avoir diminuée";
 	}
+	
+	
+	@Override
+	public String toString() {
+		return "Romain [nom=" + nom + ", force=" + force + "]";
+	}
+
 	public static void main(String[]args) {
 		Romain minus = new Romain("Minus", 6);
 		System.out.println(minus);
